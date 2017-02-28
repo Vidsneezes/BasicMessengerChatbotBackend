81f8cfd34192774f7e5069b71086fe4cdbbd15b8
Vidsneezes <moraleszez@gmail.com>
2017-02-28 07:49:14 -0600

    sets back end for greeting bot

--- app.js
+++ app.js
@@ -23,3 +23,66 @@ app.get("/webhook",function(req,res){
       res.sendStatus(403);
   }
 });
+
+//All callbacks for Messenger will be POST-ed here
+app.post("/webhook",function(req,res){
+  //Make sure this is the page subscription
+  if(req.body.object == "page"){
+    //iterate  over each entry
+    //there may be multiple entries if batched
+    req.body.entry.forEach(function(entry){
+        //iterate over each messaging event
+        entry.messaging.forEach(function(event){
+            if(event.postback){
+                processPostBack(event);
+            }
+        });
+    });
+
+    res.sendStatus(200);
+  }
+});
+
+function processPostBack(event){
+  var senderId = event.sender.id;
+  var payload = event.postback.payload;
+
+  if(payload === "Greeting"){
+    //Get user's first name from the User Profile api
+    // and include it in the greeting
+    request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+          access_token: process.env.PAGE_ACCESS_TOKEN,
+          fields: "first_name"
+      }, function(error, response, body){
+          var greeting = "";
+          if(error){
+              console.log("Error getting user's name: " + error);
+          }else{
+              var bodyObj = JSON.parse(body);
+              name = bodyObj.first_name;
+              greeting = "Hi " + name + ".";
+          }
+          var message = greeting + "My name is Raspa, im a dog bro/a";
+          sendMessage(senderId,{text:message});
+      }
+    });
+  }
+}
+
+function sendMessage(recipientId, message) {
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    method: "POST",
+    json: {
+        recipient: {id: recipientId},
+        message: message
+    }
+  }, function(error, response, body) {
+    if(error){
+        console.log("Error sending message: " + response.error);
+    }
+  });
+}
